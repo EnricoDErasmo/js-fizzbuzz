498b28ea72b232c35979633d0493e0d4482331c2
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-02-26 16:43:02 +0100

    Created conditions in js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,32 @@ e per i multipli di 5 stampi “Buzz”. Per i numeri che sono
 let listEl = document.getElementById ("list");
 
 // Creo un ciclo
-for (let i = 0; i < 100; i++) {
-    
+for (let i = 1; i < 100; i++) {
+
+    // Creo un nuovo elemento
+    let newLiElement = document.createElement("li");
+
+    newLiElement.style.listStyle = "none";
+
+    // Faccio in modo che l'elemento creato venga inserito all'interno della ul
+    listEl.append(newLiElement);
+
+    // Assegno un numero crescente a ogni li
+    newLiElement.innerHTML = i;
+
+    // Pongo le condzioni
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        newLiElement.innerHTML = "FizzBuzz";
+
+    } else if (i % 5 == 0) {
+
+        newLiElement.innerHTML = "Buzz";
+
+    } else if (i % 3 == 0) {
+
+        newLiElement.innerHTML = "Fizz";
+    }
 }
+
+
