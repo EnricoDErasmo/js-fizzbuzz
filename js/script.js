36013b7bd7239c9d57a0f5bfc82046614d9e1647
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-02-26 15:41:08 +0100

    created a variable created a loop

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”. Per i numeri che sono
+ sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+// Creo una variabile per bersagliare l'elemento HTML
+let listEl = document.getElementById ("list");
+
+// Creo un ciclo
+for (let i = 0; i < 100; i++) {
+    
+}
